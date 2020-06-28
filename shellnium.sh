e1c03ecea7cd2acc73da92193a39c101cc817b03
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2020-06-28 19:30:48 +0900

    Remove unused function.

--- shellnium.sh
+++ shellnium.sh
@@ -1,5 +1,29 @@
 #!/usr/bin/env bash
 
+main() {
+    screen_clear
+    navigate_to 'https://google.co.jp'
+
+    local searchBox=$(find_element 'name' 'q')
+    set_background_image
+
+    # 入力している感を出すため1文字ずつ入力
+    local words=('タ' 'ピ' 'オ' 'カ')
+    for word in ${words[@]}; do
+        send_keys $searchBox $word
+        set_background_image
+    done
+
+    # ENTER
+    send_keys $searchBox '\n'
+    set_background_image
+
+    for i in `seq 0 100 1000`; do
+        exec_sync_script "window.scroll(0,$i)"
+        set_background_image
+    done
+}
+
 ROOT=http://localhost:9515
 POST='curl -s -X POST -H "Content-Type: application/json"'
 PATH_SCREENSHOT=$(pwd)/image.jpg
@@ -9,20 +33,18 @@ get_session_id() {
         "desiredCapabilities": {
             "browserName": "chrome",
             "chromeOptions": {
-                "args": [ "--headless" ]
+                "args": ["--headless"]
             }
         }
     }' \
     ${ROOT}/session| jq -r '.sessionId'
 }
+
 SESSION_ID=$(get_session_id)
 BASE_URL=${ROOT}/session/${SESSION_ID}
 
-rect() {
-    $POST -d '{"height": 1000,"width":200, "x":0, "y":0}' $BASE_URL/window/rect >/dev/null
-}
-
-navigate_to() { local url=$1
+navigate_to() {
+    local url=$1
     $POST -d '{"url":"'${url}'"}' ${BASE_URL}/url >/dev/null
 }
 
@@ -30,13 +52,6 @@ find_element() {
     local property=$1
     local value=$2
     $POST -d '{"using":"'$property'", "value": "'$value'"}' ${BASE_URL}/element | jq -r '.value.ELEMENT'
-}
-
-find_elements() {
-    local property=$1
-    local value=$2
-    echo  "$property"
-    $POST -d '{"using":"'$property'", "value": "'$value'"}' ${BASE_URL}/elements
 }
 
 # []のリスト型で渡すこと
@@ -53,11 +68,11 @@ click() {
 }
 
 screenshot() {
-    curl -s -X GET $BASE_URL/screenshot | jq -r '.value' | base64 -d > $PATH_SCREENSHOT
+    curl -s -X GET ${BASE_URL}/screenshot | jq -r '.value' | base64 -d > $PATH_SCREENSHOT
 }
 
 exec_sync_script() {
-    $POST -d '{"script": "'$1'", "args":[]}' ${BASE_URL}/execute/sync >/dev/null
+    $POST -d '{"script": "'$1'", "args":['"$2"']}' ${BASE_URL}/execute/sync >/dev/null
 }
 
 iterm_set_background_image() {
@@ -73,34 +88,13 @@ EOF
 }
 
 set_background_image() {
+    screenshot
     iterm_set_background_image $PATH_SCREENSHOT
 }
 
 screen_clear() {
     clear
     iterm_set_background_image ''
-}
-
-main() {
-    screen_clear
-    navigate_to 'https://google.co.jp'
-
-    local searchBox=$(find_element 'name' 'q')
-    screenshot && set_background_image
-
-    local words=('タ' 'ピ' 'オ' 'カ')
-    for word in ${words[@]}; do
-        send_keys $searchBox $word
-        screenshot && set_background_image
-    done
-
-    send_keys $searchBox '\n'
-    screenshot && set_background_image
-
-    for i in `seq 0 100 1000`; do
-        exec_sync_script "window.scroll(0,$i)"
-        screenshot && set_background_image
-    done
 }
 
 main
