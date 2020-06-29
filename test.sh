2244bb532058707b05bfcea1533894ebcb4a5e71
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2020-06-29 17:24:08 +0900

    Remove rect

--- test.sh
+++ test.sh
@@ -2,16 +2,15 @@
 source ./selenium.sh
 
 main() {
-    curl -s -X POST -H 'Content-Type: application/json' -d '{"width": 750,"height": 1000}' ${BASE_URL}/window/rect
     # Googleのトップページに遷移
     navigate_to 'https://google.co.jp'
 
     # 検索ボックスの要素を取得
     local searchBox=$(find_element 'name' 'q')
+    echo "要素ID: $searchBox"
 
     # 検索ボックスに入力＆検索実行
     send_keys $searchBox "タピオカ\n"
 }
 
 main
-
